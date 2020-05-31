6d761757678df287995b0e7c2c85a1e2cb4974bf
Akhil <akhil.alfons.k@gmail.com>
2020-05-31 11:16:01 +0100

    More updates & tweaks

--- src/main/java/com/causefinder/routemonitor/client/BigQueryClient.java
+++ src/main/java/com/causefinder/routemonitor/client/BigQueryClient.java
@@ -20,32 +20,35 @@ public class BigQueryClient {
     private String datasetName;
     @Value("${gcp.tripdb.table}")
     private String tableName;
-
+    @Value("${gcp.tripdb.senddata.enabled}")
+    private boolean sendDataEnabled;
     @Autowired
     private BigQuery bigQueryClient;
 
     private ObjectMapper oMapper = new ObjectMapper();
 
     public void pushDataToGCP(List<StopEvent> stopEvents) {
-        TableId tableId = TableId.of(datasetName, tableName);
-        InsertAllRequest.Builder builder = InsertAllRequest.newBuilder(tableId);
-        stopEvents.stream().forEach(stopEvent -> {
-            Map<String, Object> datMap = oMapper.convertValue(stopEvent, Map.class);
-            datMap.put("aimedArrivalTime", Timestamp.of(stopEvent.getAimedArrivalTime()).toString());
-            datMap.put("expectedArrivalTime", Timestamp.of(stopEvent.getExpectedArrivalTime()).toString());
-            datMap.put("aimedDepartureTime", Timestamp.of(stopEvent.getAimedDepartureTime()).toString());
-            datMap.put("expectedDepartureTime", Timestamp.of(stopEvent.getExpectedDepartureTime()).toString());
-            datMap.put("recordedAtTime", Timestamp.of(stopEvent.getRecordedAtTime()).toString());
-            builder.addRow(datMap);
-        });
-        InsertAllResponse response = bigQueryClient.insertAll(builder.build());
-        if (response.hasErrors()) {
-            for (Map.Entry<Long, List<BigQueryError>> entry : response.getInsertErrors().entrySet()) {
-                StringBuilder errorString = new StringBuilder("Error in Pushing data to GCP:");
-                errorString.append("[").append(entry.getKey()).append("=>[");
-                entry.getValue().stream().forEach(err -> errorString.append(err.getMessage()).append(","));
-                errorString.append("]]");
-                log.warn("Error in Pushing data to GCP: {} ", errorString.toString());
+        if (sendDataEnabled) {
+            TableId tableId = TableId.of(datasetName, tableName);
+            InsertAllRequest.Builder builder = InsertAllRequest.newBuilder(tableId);
+            stopEvents.stream().forEach(stopEvent -> {
+                Map<String, Object> datMap = oMapper.convertValue(stopEvent, Map.class);
+                datMap.put("aimedArrivalTime", Timestamp.of(stopEvent.getAimedArrivalTime()).toString());
+                datMap.put("expectedArrivalTime", Timestamp.of(stopEvent.getExpectedArrivalTime()).toString());
+                datMap.put("aimedDepartureTime", Timestamp.of(stopEvent.getAimedDepartureTime()).toString());
+                datMap.put("expectedDepartureTime", Timestamp.of(stopEvent.getExpectedDepartureTime()).toString());
+                datMap.put("recordedAtTime", Timestamp.of(stopEvent.getRecordedAtTime()).toString());
+                builder.addRow(datMap);
+            });
+            InsertAllResponse response = bigQueryClient.insertAll(builder.build());
+            if (response.hasErrors()) {
+                for (Map.Entry<Long, List<BigQueryError>> entry : response.getInsertErrors().entrySet()) {
+                    StringBuilder errorString = new StringBuilder("Error in Pushing data to GCP:");
+                    errorString.append("[").append(entry.getKey()).append("=>[");
+                    entry.getValue().stream().forEach(err -> errorString.append(err.getMessage()).append(","));
+                    errorString.append("]]");
+                    log.warn("Error in Pushing data to GCP: {} ", errorString.toString());
+                }
             }
         }
     }
